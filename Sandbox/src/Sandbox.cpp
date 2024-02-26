ac9e279c83a32e07bb05020a154c07035e01b994
andrew3467 <apgraser2@gmail.com>
2024-02-26 18:54:58 -0500

    Setup Project Structure

---
+++ Sandbox/src/Sandbox.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Andrew Graser on 2/26/2024.
+//
+
+#include "Test.h"
+
+
+int main() {
+    Test::Print("Testing");
+
+    return 0;
+}
---
+++ Wolframite/src/Test.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+namespace Test {
+	void Print(const char* str) {
+		std::cout << str;
+	}
+}
