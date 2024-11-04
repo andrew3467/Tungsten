19d394aced350c0f8e9ee6a8b60d72a0be93754e
andrew3467 <apgraser2@gmail.com>
2024-11-04 16:46:03 -0500

    Added Logger

---
+++ src/Tungsten/Core/Logger.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Andrew Graser on 11/4/2024.
+//
+
+#include "Logger.h"
+
+
+
+namespace Tungsten {
+
+}
---
+++ src/Tungsten/Core/Logger.h
@@ -0,0 +1,17 @@
+//
+// Created by Andrew Graser on 11/4/2024.
+//
+
+
+#pragma once
+#include <iostream>
+
+void print() {
+    std::cout << "Hello World!\n";
+}
+
+namespace Tungsten {
+    class Logger {
+
+    };
+}
